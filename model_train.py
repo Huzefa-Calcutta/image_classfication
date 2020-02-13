011fa225514644f83a42a7537df3a931c4107488
Huzefa Calcuttawala <huzefa.calcuttawala@here.com>
2020-02-13 17:35:16 +0530

    Added inference file and made model more configurable

--- model_train.py
+++ model_train.py
@@ -12,8 +12,7 @@ import cv2
 import math
 import json
 import numpy as np
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+from model import *
 
 
 def focal_loss(alpha, gamma):
@@ -52,7 +51,7 @@ def image_data_gen(batch_size, image_dir, label_img_name_dict, img_type =".jpeg"
             yield batch_x, batch_y
 
 
-config_file = sys.argv[1]
+config_file = "model_train.cfg"
 cfg_parser = configparser.ConfigParser()
 cfg_parser.read(config_file)
 
@@ -74,47 +73,14 @@ with open(val_label_image_json, 'r') as inp:
 
 batch_size = int(cfg_parser.get('model_specs', 'batch_size'))
 num_epochs = int(cfg_parser.get("model_specs", "epochs"))
+optimizer = cfg_parser.get("model_specs", "optimizer")
+learning_rate = float(cfg_parser.get("model_specs", "learning_rate"))
+gpu = cfg_parser.get("model_specs", "gpu")
+os.environ["CUDA_VISIBLE_DEVICES"] = gpu
+
+image_classifier = Image_Classifier(0, [], [], 'mobilenet_v2', optimizer, learning_rate)
 
 number_of_train_images = len([img for img in os.listdir(train_img_folder) if img.endswith(img_type)])
 number_of_test_images = len([img for img in os.listdir(val_img_folder) if img.endswith(img_type)])
 
-x = Input(shape=(None, None, 3))
-feat_extractor = MobileNetV2(include_top=False, input_tensor=x)
-x = feat_extractor.output
-for layer in feat_extractor.layers[:-10]:
-    layer.trainable = False
-
-x = GlobalAveragePooling2D()(x)
-x = Dense(4, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=regularizers.l2(0.01), bias_regularizer=regularizers.l1(0.02))(x)
-pred_output = Activation('softmax')(x)
-classifier = Model(inputs=feat_extractor.input, outputs=pred_output)
-classifier.compile(optimizers.Adagrad(lr=0.0001), loss=focal_loss(0.4, 2.0))
-
-for layer in classifier.layers:
-    if layer.trainable:
-        print(layer)
-
-earlystop = EarlyStopping(monitor='val_loss', patience=100, mode='min', restore_best_weights=True)  # ensures that we have model weights corresponding to the best value of the metric at the end of
-
-# make tensorbaord log_dir
-if not os.path.exists("logs"):
-    os.mkdir("logs")
-
-if not os.path.exists(model_save_dir):
-    os.makedirs(model_save_dir)
-
-tensorboard = TensorBoard(log_dir='./logs', write_graph=True, update_freq='epoch')
-
-# Saving model_checkpoint
-filepath = os.path.join(model_save_dir, model_weights_file_name + "-{epoch:02d}-{val_loss:.2f}.h5")
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,
-                             save_best_only=True,
-                             mode='min', save_weights_only=True)
-
-history = classifier.fit_generator(image_data_gen(batch_size, train_img_folder, train_labels, img_type), epochs=num_epochs,
-                                           steps_per_epoch=math.ceil(number_of_train_images/num_epochs),
-                                           validation_data=image_data_gen(batch_size, val_img_folder, val_labels, img_type),
-                                           validation_steps=math.ceil(number_of_test_images/batch_size),
-                                           callbacks=[checkpoint, earlystop, tensorboard], verbose=2,
-                                           shuffle=False,
-                                           use_multiprocessing=True, workers=8)
+image_classifier.train_model(img_type, train_img_folder, train_labels, val_img_folder, val_labels, model_save_dir, model_weights_file_name, batch_size, num_epochs)
